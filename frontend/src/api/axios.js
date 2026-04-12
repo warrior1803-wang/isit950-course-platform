000355fe937863ff766ec6e7eeae4be321032f2c
Bingyan Wang <bw349@uowmail.edu.au>
2026-04-12 12:35:26 +1000

    :fix fix front end authtication issues and change port address

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -7,10 +7,19 @@ const api = axios.create({
   },
 });
 
-// Attach stored token to every request
-const token = localStorage.getItem('token');
-if (token) {
-  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-}
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(null, error => {
+  if (error.response?.status === 401) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('auth_user');
+    window.location.href = '/login';
+  }
+  return Promise.reject(error);
+});
 
 export default api;
