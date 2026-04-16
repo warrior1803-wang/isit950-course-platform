3f3abc69644350021ec7d5ebcfabd64be8e5edce
Mingcan Yang <83692733+MingcanYang@users.noreply.github.com>
2026-04-17 01:09:25 +1000

    Add PostRequest.java

---
+++ backend/src/main/java/com/learningplatform/backend/dto/PostRequest.java
@@ -0,0 +1,12 @@
+package com.learningplatform.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostRequest {
+    private String title;
+    private String body;
+    private String content;
+}
