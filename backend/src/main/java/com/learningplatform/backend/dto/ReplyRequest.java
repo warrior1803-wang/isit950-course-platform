5ebed7933082eba8ee3cad9988ee18a0319d750e
Mingcan Yang <83692733+MingcanYang@users.noreply.github.com>
2026-04-17 01:09:25 +1000

    Add ReplyRequest.java

---
+++ backend/src/main/java/com/learningplatform/backend/dto/ReplyRequest.java
@@ -0,0 +1,11 @@
+package com.learningplatform.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReplyRequest {
+    private String body;
+    private String content;
+}
